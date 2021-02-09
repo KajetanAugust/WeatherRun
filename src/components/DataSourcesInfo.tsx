d44bdeb095094fe9c2bc063cb4854b1887d78866
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2021-02-09 15:10:37 +0100

    feat: added icons for dark and light mode in data sources, added mapbox icon

--- src/components/DataSourcesInfo.tsx
+++ src/components/DataSourcesInfo.tsx
@@ -1,26 +1,38 @@
-import React from "react";
+import React, {useContext} from "react";
 
 import aqicnLogo from '../logos/aqilogo.png'
-import openWeatherLogo from '../logos/openweatherlogo-dark.png'
+import openweatherdark from '../logos/openweatherlogo-dark.png'
+import openweatherlight from '../logos/openweatherlogo-light.png'
+import mapboxdark from '../logos/mapbox-dark.svg'
+import mapboxlight from '../logos/mapbox-light.svg'
+
+import {ThemeContext} from "../contexts";
 
 import Nav from "./Nav";
 
-export default function DataSourcesInfo () {
 
+export default function DataSourcesInfo () {
+    const { theme } = useContext(ThemeContext);
     return (
         <React.Fragment>
             <Nav location={''}/>
             <div className='data-sources-info'>
                 <div>
-                    <h3>Weather information were provided by OpenWeatherMap</h3>
+                    <h3>Weather information provided by OpenWeatherMap</h3>
                     <a href='https://openweathermap.org/' target='_blank' rel='noreferrer'>
-                        <img src={openWeatherLogo} alt='OpenWeather Logo'/>
+                        <img src={theme === 'light' ? openweatherdark : openweatherlight} alt='OpenWeather Logo'/>
                     </a>
                 </div>
                 <div>
-                    <h3>Air Quality information were provided by AQICN</h3>
+                    <h3>Air Quality information provided by AQICN</h3>
                     <a href='https://aqicn.org/' target='_blank' rel='noreferrer'>
                         <img src={aqicnLogo} alt='AQICN Logo'/>
+                    </a>
+                </div>
+                <div>
+                    <h3>Coordinates provided by Mapbox</h3>
+                    <a href='https://www.mapbox.com/' target='_blank' rel='noreferrer'>
+                        <img src={theme === 'light' ? mapboxdark : mapboxlight} alt='Mapbox Logo'/>
                     </a>
                 </div>
             </div>
