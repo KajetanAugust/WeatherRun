ea7bbc9f40d08058641644094eef64ccab0653c3
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2021-03-19 23:22:07 +0100

    feat: added nivo library for displaying charts in ModalWindow, started experimenting with different days to show data

--- src/components/ModalWindow.tsx
+++ src/components/ModalWindow.tsx
@@ -1,5 +1,6 @@
-import React, {useContext, useState} from "react";
+import React, {useContext} from "react";
 import {Paper, Modal, Button} from "@material-ui/core";
+import { ResponsiveLine } from '@nivo/line'
 
 import {getFormattedTime} from "../utils/dateFormatters";
 import {ThemeContext} from "../contexts";
@@ -27,16 +28,84 @@ export default function ModalWindow (props: ModalWindowProps) {
                     style={{position:'absolute', top:'15px', right:'15px'}}
                     onClick={ () => props.openSetter(false) }
                 >X</Button>
-                <div>
-                    <table style={{border: '1px solid lightgray'}}>
-                    {
-                        props.weather.hourly.slice(0, 6).map( (hour: Record<any, any>) => <tr style={{border: '1px solid lightgray'}}><td style={{border: '1px solid lightgray'}}>{getFormattedTime(hour.dt)}</td><td style={{border: '1px solid lightgray'}}>{Math.round(hour.temp)}&deg;C </td></tr>)
-                    }
-                    </table>
-                </div>
-                <div>
+                {/*<div>*/}
+                {/*    <table style={{border: '1px solid lightgray'}}>*/}
+                {/*    {*/}
+                {/*        props.weather.hourly.slice(0, 6).map( (hour: Record<any, any>) => <tr style={{border: '1px solid lightgray'}}><td style={{border: '1px solid lightgray'}}>{}</td><td style={{border: '1px solid lightgray'}}>{</td></tr>)*/}
+                {/*    }*/}
+                {/*    </table>*/}
+                {/*</div>*/}
 
-                </div>
+                <ResponsiveLine
+                    data={[
+                        {
+                            "id": "temperature",
+                            "color": "#9CDE9F",
+                            "data": props.weather.hourly.slice(0, 12).map((hour: Record<any, any>) => ({'x': `${getFormattedTime(hour.dt)}`, 'y': `${Math.round(hour.temp)}&deg;C }`}))
+                        },
+                        {
+                            "id": "wind speed",
+                            "color": "#F5B82E",
+                            "data": props.weather.hourly.slice(0, 12).map((hour: Record<any, any>) => ({'x': `${getFormattedTime(hour.dt)}`, 'y': `${Math.ceil(hour.wind_speed * 3.6)}km/h`}))
+                        }
+                    ]}
+                    margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+                    xScale={{ type: 'point' }}
+                    yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
+                    yFormat=" >-.2f"
+                    axisTop={null}
+                    axisRight={null}
+                    axisBottom={{
+                        orient: 'bottom',
+                        tickSize: 5,
+                        tickPadding: 5,
+                        tickRotation: 0,
+                        legend: 'transportation',
+                        legendOffset: 36,
+                        legendPosition: 'middle'
+                    }}
+                    axisLeft={{
+                        orient: 'left',
+                        tickSize: 5,
+                        tickPadding: 5,
+                        tickRotation: 0,
+                        legend: 'count',
+                        legendOffset: -40,
+                        legendPosition: 'middle'
+                    }}
+                    pointSize={10}
+                    pointColor={{ theme: 'background' }}
+                    pointBorderWidth={2}
+                    pointBorderColor={{ from: 'serieColor' }}
+                    pointLabelYOffset={-12}
+                    useMesh={true}
+                    legends={[
+                        {
+                            anchor: 'bottom-right',
+                            direction: 'column',
+                            justify: false,
+                            translateX: 100,
+                            translateY: 0,
+                            itemsSpacing: 0,
+                            itemDirection: 'left-to-right',
+                            itemWidth: 80,
+                            itemHeight: 20,
+                            itemOpacity: 0.75,
+                            symbolSize: 12,
+                            symbolShape: 'circle',
+                            symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                            effects: [
+                                {
+                                    on: 'hover',
+                                    style: {
+                                        itemBackground: 'rgba(0, 0, 0, .03)',
+                                        itemOpacity: 1
+                                    }
+                                }
+                            ]
+                        }
+                    ]}
+                />
             </Paper>
         </Modal>
 
