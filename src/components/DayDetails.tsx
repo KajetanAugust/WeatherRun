0c2760fd39789fc6d5b5788e9cc4902cf5e6f2d4
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2021-03-29 22:39:44 +0200

    feat: started working on DayDetails.tsx component. It is accessible through the button in the nav. For now it only displays graph with temperature data for next 24 hours.

--- src/components/DayDetails.tsx
+++ src/components/DayDetails.tsx
@@ -1,99 +1,30 @@
-// import React, {useContext, useEffect, useState} from "react";
 import React from "react";
 import {Paper} from "@material-ui/core";
-// import { ResponsiveLine } from '@nivo/line'
-//
-// import {getFormattedTime} from "../utils/dateFormatters";
-
 
 import Nav from "./Nav";
 import Loading from "./Loading";
+import GraphCreator from "./GraphCreator"
+import formatLocation from "../utils/formatLocation";
 
 interface DayDetailsTypes {
     pollution: Record<any, any>,
-    weather: Record<any, any>
+    weather: Record<any, any>,
+    locationInfo: Record<any, any>
 }
 
 export default function DayDetails (props: DayDetailsTypes) {
 
-    props.weather.hourly && console.log(props.weather)
-
+    console.log(props.pollution)
     return (
-            // <div>In Development</div>
+
+
+        // TODO: find a way to fix error occurring when Day Details site is refreshed
+
         props.weather !== {} ?
         <React.Fragment>
-            <Nav location={''}/>
-            <Paper style={{width: '500px', height:'500px'}}>
-                test
-                {/*<ResponsiveLine*/}
-                {/*    data={[*/}
-                {/*        {*/}
-                {/*            "id": "temperature",*/}
-                {/*            "color": "#9CDE9F",*/}
-                {/*            // @ts-ignore*/}
-                {/*            "data": props.weather.hourly.slice(0, 12).map((hour: Record<any, any>) => ({*/}
-                {/*                'x': `${getFormattedTime(hour.dt)}`,*/}
-                {/*                'y': `${Math.round(hour.temp)}&deg;C }`*/}
-                {/*            }))*/}
-                {/*        }*/}
-                {/*    ]}*/}
-                {/*    margin={{top: 50, right: 110, bottom: 50, left: 60}}*/}
-                {/*    xScale={{type: 'point'}}*/}
-                {/*    yScale={{type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false}}*/}
-                {/*    yFormat=" >-.2f"*/}
-                {/*    axisTop={null}*/}
-                {/*    axisRight={null}*/}
-                {/*    axisBottom={{*/}
-                {/*        orient: 'bottom',*/}
-                {/*        tickSize: 5,*/}
-                {/*        tickPadding: 5,*/}
-                {/*        tickRotation: 0,*/}
-                {/*        legend: 'transportation',*/}
-                {/*        legendOffset: 36,*/}
-                {/*        legendPosition: 'middle'*/}
-                {/*    }}*/}
-                {/*    axisLeft={{*/}
-                {/*        orient: 'left',*/}
-                {/*        tickSize: 5,*/}
-                {/*        tickPadding: 5,*/}
-                {/*        tickRotation: 0,*/}
-                {/*        legend: 'count',*/}
-                {/*        legendOffset: -40,*/}
-                {/*        legendPosition: 'middle'*/}
-                {/*    }}*/}
-                {/*    pointSize={10}*/}
-                {/*    pointColor={{theme: 'background'}}*/}
-                {/*    pointBorderWidth={2}*/}
-                {/*    pointBorderColor={{from: 'serieColor'}}*/}
-                {/*    pointLabelYOffset={-12}*/}
-                {/*    useMesh={true}*/}
-                {/*    legends={[*/}
-                {/*        {*/}
-                {/*            anchor: 'bottom-right',*/}
-                {/*            direction: 'column',*/}
-                {/*            justify: false,*/}
-                {/*            translateX: 100,*/}
-                {/*            translateY: 0,*/}
-                {/*            itemsSpacing: 0,*/}
-                {/*            itemDirection: 'left-to-right',*/}
-                {/*            itemWidth: 80,*/}
-                {/*            itemHeight: 20,*/}
-                {/*            itemOpacity: 0.75,*/}
-                {/*            symbolSize: 12,*/}
-                {/*            symbolShape: 'circle',*/}
-                {/*            symbolBorderColor: 'rgba(0, 0, 0, .5)',*/}
-                {/*            effects: [*/}
-                {/*                {*/}
-                {/*                    on: 'hover',*/}
-                {/*                    style: {*/}
-                {/*                        itemBackground: 'rgba(0, 0, 0, .03)',*/}
-                {/*                        itemOpacity: 1*/}
-                {/*                    }*/}
-                {/*                }*/}
-                {/*            ]*/}
-                {/*        }*/}
-                {/*    ]}*/}
-                {/*/>*/}
+            <Nav location={formatLocation(props.locationInfo)}/>
+            <Paper style={{width: '1000px', height:'300px'}}>
+                <GraphCreator weather={props.weather}/>
             </Paper>
         </React.Fragment>
             :
