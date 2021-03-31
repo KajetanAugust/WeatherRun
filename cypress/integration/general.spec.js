6590a03223cbab1968d74d5a2ece26da8d9248c8
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2021-03-31 22:15:35 +0200

    refactor: moved all tests to one file

--- cypress/integration/general.spec.js
+++ cypress/integration/general.spec.js
@@ -1,14 +1,14 @@
-describe('General tests', () => {
-
-    it('visits the app', () => {
-        // eslint-disable-next-line no-undef
-        cy.visit('/')
-    })
-
-    it('app changes theme', () => {
-        // eslint-disable-next-line no-undef
-        cy.get('.PrivateSwitchBase-input-4')
-            .click()
-    })
-})
+// describe('General tests', () => {
+//
+//     it('visits the app', () => {
+//         // eslint-disable-next-line no-undef
+//         cy.visit('/')
+//     })
+//
+//     it('app changes theme', () => {
+//         // eslint-disable-next-line no-undef
+//         cy.get('.PrivateSwitchBase-input-4')
+//             .click()
+//     })
+// })
 
--- cypress/integration/results.spec.js
+++ cypress/integration/results.spec.js
@@ -1,63 +1,63 @@
-describe('Results', () => {
-    beforeEach(() => {
-        // eslint-disable-next-line no-undef
-        cy.visit('/')
-    })
-
-    it('site info is shown', () => {
-        const input = 'Katowice'
-        // eslint-disable-next-line no-undef
-        cy.get('.search-form')
-        // eslint-disable-next-line no-undef
-        cy.get('.MuiOutlinedInput-inputMarginDense')
-            .type(input)
-        // eslint-disable-next-line no-undef
-        cy.get('.search-link')
-            .click()
-        // eslint-disable-next-line no-undef
-        cy.get('.footer')
-            .find('button')
-            .click()
-        // eslint-disable-next-line no-undef
-        cy.url().should('eq', 'http://localhost:3000/info')
-    })
-
-    it('github link/button is present', () => {
-        const input = 'Katowice'
-        // eslint-disable-next-line no-undef
-        cy.get('.search-form')
-        // eslint-disable-next-line no-undef
-        cy.get('.MuiOutlinedInput-inputMarginDense')
-            .type(input)
-        // eslint-disable-next-line no-undef
-        cy.get('.search-link')
-            .click()
-        // eslint-disable-next-line no-undef
-        cy.get('.footer')
-            .find('button')
-            .click()
-            // eslint-disable-next-line no-undef
-            cy.get('.github-link')
-                .should('have.attr', 'target', '_blank')
-                .should('have.attr', 'href', 'https://github.com/KajetanAugust/WeatherRun')
-    })
-
-    it('goes back to search screen', () => {
-        const input = 'Katowice'
-        // eslint-disable-next-line no-undef
-        cy.get('.search-form')
-        // eslint-disable-next-line no-undef
-        cy.get('.MuiOutlinedInput-inputMarginDense')
-            .type(input)
-        // eslint-disable-next-line no-undef
-        cy.get('.search-link')
-            .click()
-        // eslint-disable-next-line no-undef
-        cy.get('.back-arrow')
-            .click()
-        // eslint-disable-next-line no-undef
-        cy.url().should('eq', 'http://localhost:3000/')
-    })
-})
-
-
+// describe('Results', () => {
+//     beforeEach(() => {
+//         // eslint-disable-next-line no-undef
+//         cy.visit('/')
+//     })
+//
+//     it('site info is shown', () => {
+//         const input = 'Katowice'
+//         // eslint-disable-next-line no-undef
+//         cy.get('.search-form')
+//         // eslint-disable-next-line no-undef
+//         cy.get('.MuiOutlinedInput-inputMarginDense')
+//             .type(input)
+//         // eslint-disable-next-line no-undef
+//         cy.get('.search-link')
+//             .click()
+//         // eslint-disable-next-line no-undef
+//         cy.get('.footer')
+//             .find('button')
+//             .click()
+//         // eslint-disable-next-line no-undef
+//         cy.url().should('eq', 'http://localhost:3000/info')
+//     })
+//
+//     it('github link/button is present', () => {
+//         const input = 'Katowice'
+//         // eslint-disable-next-line no-undef
+//         cy.get('.search-form')
+//         // eslint-disable-next-line no-undef
+//         cy.get('.MuiOutlinedInput-inputMarginDense')
+//             .type(input)
+//         // eslint-disable-next-line no-undef
+//         cy.get('.search-link')
+//             .click()
+//         // eslint-disable-next-line no-undef
+//         cy.get('.footer')
+//             .find('button')
+//             .click()
+//             // eslint-disable-next-line no-undef
+//             cy.get('.github-link')
+//                 .should('have.attr', 'target', '_blank')
+//                 .should('have.attr', 'href', 'https://github.com/KajetanAugust/WeatherRun')
+//     })
+//
+//     it('goes back to search screen', () => {
+//         const input = 'Katowice'
+//         // eslint-disable-next-line no-undef
+//         cy.get('.search-form')
+//         // eslint-disable-next-line no-undef
+//         cy.get('.MuiOutlinedInput-inputMarginDense')
+//             .type(input)
+//         // eslint-disable-next-line no-undef
+//         cy.get('.search-link')
+//             .click()
+//         // eslint-disable-next-line no-undef
+//         cy.get('.back-arrow')
+//             .click()
+//         // eslint-disable-next-line no-undef
+//         cy.url().should('eq', 'http://localhost:3000/')
+//     })
+// })
+//
+//
