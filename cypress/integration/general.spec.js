e49b607278ea99f45ee42bd35910d9c3c9184cc8
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2021-03-31 16:55:14 +0200

    feat: started adding tests to the app

---
+++ cypress/integration/general.spec.js
@@ -0,0 +1,14 @@
+describe('General tests', () => {
+
+    it('visits the app', () => {
+        // eslint-disable-next-line no-undef
+        cy.visit('/')
+    })
+
+    it('app changes theme', () => {
+        // eslint-disable-next-line no-undef
+        cy.get('.PrivateSwitchBase-input-4')
+            .click()
+    })
+})
+
