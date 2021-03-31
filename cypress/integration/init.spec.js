90b02ce8378d5bcef63dae8cb0e77e1ee24c7f0a
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2021-03-31 22:15:35 +0200

    refactor: moved all tests to one file

--- cypress/integration/init.spec.js
+++ cypress/integration/init.spec.js
@@ -5,4 +5,167 @@ describe('Cypress', () => {
     })
 })
 
+describe('General tests', () => {
+
+    it('visits the app', () => {
+        // eslint-disable-next-line no-undef
+        cy.visit('/')
+    })
+
+    it('app changes theme', () => {
+        // eslint-disable-next-line no-undef
+        cy.get('.PrivateSwitchBase-input-4')
+            .click()
+    })
+})
+
+describe('Search form tests', () => {
+    beforeEach(() => {
+        // eslint-disable-next-line no-undef
+        cy.visit('/')
+    })
+
+    it('focuses the input', () => {
+        // eslint-disable-next-line no-undef
+        cy.focused().should('have.class', 'MuiOutlinedInput-inputMarginDense')
+    })
+
+    it('accepts input', () => {
+        const input = 'Katowice'
+        // eslint-disable-next-line no-undef
+        cy.get('.MuiOutlinedInput-inputMarginDense')
+            .type(input)
+            .should('have.value', input)
+    })
+
+    it('submit locked for input under three letters', () => {
+        const input = 'te'
+        // eslint-disable-next-line no-undef
+        cy.get('.MuiOutlinedInput-inputMarginDense')
+            .type(input)
+        // eslint-disable-next-line no-undef
+        cy.get('.MuiButtonBase-root.MuiButton-root.MuiButton-outlined.Mui-disabled.Mui-disabled')
+            .click()
+        // eslint-disable-next-line no-undef
+        cy.url().should('eq', 'http://localhost:3000/')
+    })
+
+    it('submits input', () => {
+        const input = 'katowice'
+        // eslint-disable-next-line no-undef
+        cy.get('.search-form')
+        // eslint-disable-next-line no-undef
+        cy.get('.MuiOutlinedInput-inputMarginDense')
+            .type(input)
+        // eslint-disable-next-line no-undef
+        cy.get('.search-link')
+            .click()
+        // eslint-disable-next-line no-undef
+        cy.url().should('eq', 'http://localhost:3000/results?search=katowice')
+    })
+
+    it('submits input on enter click', () => {
+        const input = 'katowice'
+        // eslint-disable-next-line no-undef
+        cy.get('.search-form')
+        // eslint-disable-next-line no-undef
+        cy.get('.MuiOutlinedInput-inputMarginDense')
+            .type(input)
+            .type('{enter}')
+        // eslint-disable-next-line no-undef
+        cy.url().should('eq', 'http://localhost:3000/results?search=katowice')
+    })
+
+
+    it('clicks last search', () => {
+        const input = "Katowice"
+        // eslint-disable-next-line no-undef
+        cy.get('.search-form')
+        // eslint-disable-next-line no-undef
+        cy.get('.MuiOutlinedInput-inputMarginDense')
+            .type(input)
+        // eslint-disable-next-line no-undef
+        cy.get('.search-link')
+            .click()
+        // eslint-disable-next-line no-undef
+        cy.get('.back-arrow')
+            .click()
+        // eslint-disable-next-line no-undef
+        cy.get('.last-searches-div')
+            .find('button')
+            .click()
+        // eslint-disable-next-line no-undef
+        cy.url().should('eq', 'http://localhost:3000/results?search=katowice')
+    })
+
+    it('locates user', () => {
+        // eslint-disable-next-line no-undef
+        cy.get('.location-button')
+            .click()
+    })
+})
+
+
+describe('Results', () => {
+    beforeEach(() => {
+        // eslint-disable-next-line no-undef
+        cy.visit('/')
+    })
+
+    it('site info is shown', () => {
+        const input = 'Katowice'
+        // eslint-disable-next-line no-undef
+        cy.get('.search-form')
+        // eslint-disable-next-line no-undef
+        cy.get('.MuiOutlinedInput-inputMarginDense')
+            .type(input)
+        // eslint-disable-next-line no-undef
+        cy.get('.search-link')
+            .click()
+        // eslint-disable-next-line no-undef
+        cy.get('.footer')
+            .find('button')
+            .click()
+        // eslint-disable-next-line no-undef
+        cy.url().should('eq', 'http://localhost:3000/info')
+    })
+
+    it('github link/button is present', () => {
+        const input = 'Katowice'
+        // eslint-disable-next-line no-undef
+        cy.get('.search-form')
+        // eslint-disable-next-line no-undef
+        cy.get('.MuiOutlinedInput-inputMarginDense')
+            .type(input)
+        // eslint-disable-next-line no-undef
+        cy.get('.search-link')
+            .click()
+        // eslint-disable-next-line no-undef
+        cy.get('.footer')
+            .find('button')
+            .click()
+        // eslint-disable-next-line no-undef
+        cy.get('.github-link')
+            .should('have.attr', 'target', '_blank')
+            .should('have.attr', 'href', 'https://github.com/KajetanAugust/WeatherRun')
+    })
+
+    it('goes back to search screen', () => {
+        const input = 'Katowice'
+        // eslint-disable-next-line no-undef
+        cy.get('.search-form')
+        // eslint-disable-next-line no-undef
+        cy.get('.MuiOutlinedInput-inputMarginDense')
+            .type(input)
+        // eslint-disable-next-line no-undef
+        cy.get('.search-link')
+            .click()
+        // eslint-disable-next-line no-undef
+        cy.get('.back-arrow')
+            .click()
+        // eslint-disable-next-line no-undef
+        cy.url().should('eq', 'http://localhost:3000/')
+    })
+})
+
 
