3ecabf21ec890d5a7b391a8e52db02e082899655
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2021-04-13 23:08:56 +0200

    refactor: commented some functionality for merge

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,13 +1,13 @@
 import React, { useContext } from 'react'
-import {useHistory, useLocation} from "react-router-dom";
-
+// import {useHistory, useLocation} from "react-router-dom";
+import {useLocation} from "react-router-dom";
 import { ThemeContext } from "../../contexts";
 
 import ThemeSwitch from "./ThemeSwitch";
 import GoBackButton from "./GoBackButton";
 import MobileMenu from "./MobileMenu";
 import InfoButton from "./InfoButton";
-import {Button} from "@material-ui/core";
+// import {Button} from "@material-ui/core";
 
 
 interface NavProps {
@@ -15,8 +15,8 @@ interface NavProps {
 }
 
 export default function Nav (props: NavProps) {
-
-    let history = useHistory()
+    //COMMENTED OUT FOR COMMIT
+    // let history = useHistory()
     let location = useLocation()
     const {theme, setTheme} = useContext(ThemeContext);
 
@@ -27,19 +27,20 @@ export default function Nav (props: NavProps) {
             {
                 location.pathname !== '/info' &&
                     <div>
-                        <Button
-                            variant={theme === 'light' ? 'outlined' : "contained"}
-                            onClick={() => history.push(`/results?search=${props.location}`)}
-                            style={{marginRight: '15px'}}
-                        >
-                            Main Screen
-                        </Button>
-                        <Button
-                            variant={theme === 'light' ? 'outlined' : "contained"}
-                            onClick={() => history.push('/day-view')}
-                        >
-                            Day Details
-                        </Button>
+                        {/*TODO: COMMENTED OUT FOR COMMIT*/}
+                        {/*<Button*/}
+                        {/*    variant={theme === 'light' ? 'outlined' : "contained"}*/}
+                        {/*    onClick={() => history.push(`/results?search=${props.location}`)}*/}
+                        {/*    style={{marginRight: '15px'}}*/}
+                        {/*>*/}
+                        {/*    Main Screen*/}
+                        {/*</Button>*/}
+                        {/*<Button*/}
+                        {/*    variant={theme === 'light' ? 'outlined' : "contained"}*/}
+                        {/*    onClick={() => history.push('/day-view')}*/}
+                        {/*>*/}
+                        {/*    Day Details*/}
+                        {/*</Button>*/}
                     </div>
             }
             <div>
